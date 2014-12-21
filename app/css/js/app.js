63a799333181766454ebc36bb26724ffcda6b254
Vamsi Krishna Mannem <vamsi.mannem@gmail.com>
2014-12-21 19:35:40 +0530

    Delete app.js

--- app/css/js/app.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("text")
-.innerHTML = "Hi Friends this is a small example of the gulp and express live reload. When you are change and Save the file in app it's reload the page in browser";
